94ed7d40dfda4f57b4237879b811aaf96f4ceaa6
Miacheva Elena <mmiacheva@gmail.com>
2023-12-18 00:34:49 +0300

    Update config, get results

--- src/config.py
+++ src/config.py
@@ -1,8 +1,11 @@
-username = "postgres"
-password = "zseWQ3004"
-hostname = "localhost"
-port = "5432"
-name_database = "postgres"
+username = ""
+password = ""
+hostname = ""
+port = ""
+name_database = ""
+
+count_queries = 4
+attempts = 20
 
 queries = [
     """SELECT "VendorID", count(*) FROM "trips" GROUP BY 1""",
@@ -11,9 +14,6 @@ queries = [
     """SELECT "passenger_count", extract(year from "tpep_pickup_datetime"), round("trip_distance"), count(*) FROM "trips" GROUP BY 1, 2, 3 ORDER BY 2, 4 desc""",
 ]
 
-count_queries = 4
-attempts = 5
-
 queries_sqlite = [
     """SELECT "VendorID", count(*) FROM "trips" GROUP BY 1""",
     """SELECT "passenger_count", avg("total_amount") FROM "trips" GROUP BY 1""",
@@ -21,11 +21,11 @@ queries_sqlite = [
     """SELECT "passenger_count", STRFTIME('%Y', "tpep_pickup_datetime"), round("trip_distance"), count(*) FROM "trips" GROUP BY 1, 2, 3 ORDER BY 2, 4 desc""",
 ]
 
-data_folder = "data"
-dataset = "nyc_yellow_big.csv"
+data_folder = ""
+dataset = ""
 
-SQLITE3 = True
-PANDAS = True
-PSYCOPG2 = True
-DUCKDB = True
-SQLALCHEMY = True
+SQLITE3 = False
+PANDAS = False
+PSYCOPG2 = False
+DUCKDB = False
+SQLALCHEMY = False
